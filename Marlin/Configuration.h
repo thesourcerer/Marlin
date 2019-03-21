dcd92410536c9bdc7d6bee0024db83068ef0d81f
Eli Ofek <elofek@microsoft.com>
2019-03-21 23:16:41 +0200

    TH3D DEFAULT_MAX_FEEDRATE

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -612,7 +612,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 500, 500, 20, 25 }
+#define DEFAULT_MAX_FEEDRATE          { 500, 500, 15, 50 }
 
 /**
  * Default Max Acceleration (change/s) change = mm/s
