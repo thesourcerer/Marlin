691cc461debf729cae3208f7d5cfe4f9e2abb9b7
Eli Ofek <elofek@microsoft.com>
2019-11-07 10:52:26 +0200

    Setting Offset for HeroMe Gen 3 OEM Fan

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -786,8 +786,8 @@
  *    (0,0)
  */
 #define X_PROBE_OFFSET_FROM_EXTRUDER -40  // X offset: -left  +right  [of the nozzle]
-#define Y_PROBE_OFFSET_FROM_EXTRUDER -9  // Y offset: -front +behind [the nozzle]
-#define Z_PROBE_OFFSET_FROM_EXTRUDER 2.2   // Z offset: -below +above  [the nozzle]
+#define Y_PROBE_OFFSET_FROM_EXTRUDER -18  // Y offset: -front +behind [the nozzle]
+#define Z_PROBE_OFFSET_FROM_EXTRUDER 3.15   // Z offset: -below +above  [the nozzle]
 
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 10
