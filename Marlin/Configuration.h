7a6b235c2209eff1c9b18c381b99579df55669ef
Eli Ofek <elofek@microsoft.com>
2020-04-22 01:54:48 +0300

    Change max extrude length to 700 to allow easy loading

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -458,7 +458,7 @@
  * Note: For Bowden Extruders make this large enough to allow load/unload.
  */
 #define PREVENT_LENGTHY_EXTRUDE
-#define EXTRUDE_MAXLENGTH 600
+#define EXTRUDE_MAXLENGTH 700
 
 //===========================================================================
 //======================== Thermal Runaway Protection =======================
